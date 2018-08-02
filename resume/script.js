f75252345f697dae4ea6e0d3d2c41056dd239b3b
unknown <tianshigeily360@gmail.com>
2018-08-02 10:43:57 +0800

    changed

--- resume/script.js
+++ resume/script.js
@@ -1,3 +1,13 @@
 document.getElementById('btn').onclick = function () {
   document.getElementById('text').innerText = "你看这个面它又长又宽，就像这个碗它又大又圆"
 };
+
+var a = 1;
+
+function foo() {
+    console.log(a);
+    a = 2;
+}
+
+foo();
+console.log(a);
